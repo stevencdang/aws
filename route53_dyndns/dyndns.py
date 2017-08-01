ebe4352359f9124a32d25b9fe0959ff7bed6e9c6
Steven Dang <stevencdang@gmail.com>
2017-08-01 01:41:47 -0400

    updated with fixes

--- route53_dyndns/dyndns.py
+++ route53_dyndns/dyndns.py
@@ -24,9 +24,12 @@ def get_connection():
 
 
 def get_external_ip():
+        # Get the ip address using a specific site
+        data = urllib2.urlopen("http://checkip.dyndns.org/").read()
+        # Search the result page for an ip address
 	ip_search = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-	data = urllib2.urlopen('http://www.ipchicken.com').read()
 	ip = ip_search.findall(data)
+        # Return the result or log the error
 	if ip:
 		logger.info('found current ip to be:%s' % ip[0])
 		ip = ip[0]
@@ -36,20 +39,23 @@ def get_external_ip():
 
 
 def find_zone(zones, zone_name):
-	for zone in zones:
-		logger.debug('checking zone with ID %s and name %s\n \
-			matching zone with name %s' % (zone.id, zone.name, zone_name))
-		if zone.name == zone_name:
-			logger.debug('found zone with ID %s and name %s' % (zone.id, zone.name))
-			return zone	
+    for zone in zones:
+        logger.debug('checking zone with ID %s and name %s\n \
+                matching zone with name %s' % (zone.id, zone.name, zone_name))
+        if zone.name == zone_name:
+                logger.debug('found zone with ID %s and name %s' % (zone.id, zone.name))
+                return zone	
+        else:
+            logger.debug("Ignoring zone with name: %s" % zone)
 
 
 def define_update_record(zone_name, record_name, new_ip):
 	global cfg
+        # Connect to AWS
 	aws = get_connection()
+        # Get list of hosted zones and select the target to update base on parameters
 	zones = aws.get_zones()
 	logger.debug('Getting zone with name %s' % zone_name)
-	zone = aws.get_zone(zone_name)
 	zone = find_zone(zones, zone_name)
 	logger.debug('Found zone matching name %s with id %s' % (zone.name, zone.id))
 	logger.debug('modifying record with name %s' % record_name)
@@ -57,27 +63,28 @@ def define_update_record(zone_name, record_name, new_ip):
 	current_rrsets = aws.get_all_rrsets(zone.id)
 	current_ip = None
 	for rrset in current_rrsets:
-		if record_name in rrset.to_xml():
-			current_ip = rrset.to_xml().split('<Value>')[1].split('</Value>')[0]
-			logger.info("Current IP address is: %s" % current_ip)
+            if record_name in rrset.to_xml():
+                current_ip = rrset.to_xml().split('<Value>')[1].split('</Value>')[0]
+                logger.info("Current IP address is: %s" % current_ip)
 	updates = record.ResourceRecordSets(aws, zone.id)
 	if current_ip is not None:
-		# Delete current record with old IP
-		logger.info('existing record was found with ip %s, so adding delete before \
-			creating updated record' % (current_ip))
-		rrecord = updates.add_change('DELETE', 
-					record_name,
-					'A',
-					300
-					)
-		rrecord.add_value(current_ip)
+            # Delete current record with old IP
+            logger.info('existing record was found with ip %s, so adding delete before \
+                creating updated record' % (current_ip))
+            rrecord = updates.add_change('DELETE', 
+                                         record_name,
+                                         'A',
+                                         300
+                                         )
+            rrecord.add_value(current_ip)
 	rrecord = updates.add_change('CREATE', 
-				record_name,
-				'A',
-				300
-				)
+                                    record_name,
+                                    'A',
+                                    300
+                                    )
 	rrecord.add_value(new_ip)
 	updates.commit()
+        logger.info("Completed update to new ip: %s" % new_ip)
 
 
 if __name__ == '__main__':
